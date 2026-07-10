d302a767807152ed71c201bbbc335fd41f82af74
Nahian Mustafa <nahianbd05@gmail.com>
2026-07-11 00:10:59 +0600

    sorting fixed

--- E.c
+++ E.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include<stdlib.h>
+
 
 int main(){
 
@@ -10,25 +12,16 @@ int m=N/2;
 int arr[N];
 
 for(int i=0;i<N;i++){
-    scanf("%d",&arr[i]);}
+    scanf("%d ",&arr[i]);}
 
-    for (int i=0; i<N;i++){
-          
-         for(int j=0;j<N-1;j++)
-          {
-            if(arr[j]<arr[j+1]){continue;}
-            else{int temp = arr[j];
-                 arr[j]=arr[j+1];
-                 arr[j+1]=temp;
-            
-            }
+int compare(const void* a, const void* b)
+{
+  return (*(int *)a - *(int *)b);
+}
 
-
-          }
-    }
-
-//for(int i=0;i<N;i++){
-  //  printf("%d ",arr[i]);}
+qsort(arr,N,sizeof(int),compare);
+for(int i=0;i<N;i++){
+    printf("%d ",arr[i]);}
 
 int result=arr[m]-arr[m-1];
 
